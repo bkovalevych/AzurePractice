e4b0c64a618f8182606940a9186b43ac2af3d416
bohdan.kovalevych <bohdan.kovalevych@nure.ua>
2022-07-05 17:52:27 +0300

    set environment

--- azure-spa/src/environments/environment.prod.ts
+++ azure-spa/src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
 export const environment = {
   production: true,
-  baseUrl: ""
+  baseUrl: "https://bohdan-wallet-api.azurewebsites.net/api",
+  azureAD: {
+    clientId: "c2f0def7-e914-4b53-bdb7-46465346ff0b",
+    authority: 'https://login.microsoftonline.com/940a914e-05a6-4807-8e22-374cf7a71990',
+    redirectUri: 'http://localhost:4200/'
+  },
+  graphUrl: 'https://graph.microsoft.com/v1.0/me'
 };
