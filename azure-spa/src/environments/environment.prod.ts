009d56f93da2a85731ba6cd5992317844a33616f
bohdan.kovalevych <bohdan.kovalevych@nure.ua>
2022-07-05 20:53:29 +0300

    changed redirect url

--- azure-spa/src/environments/environment.prod.ts
+++ azure-spa/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   azureAD: {
     clientId: "c2f0def7-e914-4b53-bdb7-46465346ff0b",
     authority: 'https://login.microsoftonline.com/940a914e-05a6-4807-8e22-374cf7a71990',
-    redirectUri: 'http://localhost:4200/'
+    redirectUri: 'https://mango-hill-0cfe7f703.1.azurestaticapps.net/'
   },
   graphUrl: 'https://graph.microsoft.com/v1.0/me'
 };
